5e2f5c8639f485d3ec4d82ac890522fde56fd963
Elayt <84461884+bd-ans@users.noreply.github.com>
2022-09-08 20:10:55 +0500

    fixed mini bugs and improved performance

--- js/placeholder.js
+++ js/placeholder.js
@@ -1,7 +1,5 @@
 const searchPlaceholderSelect = document.querySelector('.js-search-select');
 const searchPlaceholderSearchInput = document.querySelector('.js-search-input');
-const searchPlaceholderSortSelect = document.querySelector('.js-sort-select');
-const elBookmarksBtnBox = document.querySelector('.bookmarks-btn-box');
 const main = document.querySelector('.main');
 
 var mainStatus = true;
@@ -11,10 +9,8 @@ window.onload = function(){
 };
 
 function placeholderRemover () {
-  elBookmarksBtnBox.classList.remove('placeholder')
   searchPlaceholderSelect.classList.remove('placeholder')
   searchPlaceholderSearchInput.classList.remove('placeholder')
-  searchPlaceholderSortSelect.classList.remove('placeholder')
   main.style.pointerEvents = 'auto';
 }
 setInterval(function () {
